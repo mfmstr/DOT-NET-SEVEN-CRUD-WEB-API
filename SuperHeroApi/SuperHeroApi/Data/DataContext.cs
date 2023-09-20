1e41acb6336667f5bb13ac62ef94bdb17572a855
mfmstr <g.kharebava01@gmail.com>
2023-09-20 21:13:22 +0400

    Adding Data folder and DataContext:DbContext class and initiated migration

---
+++ SuperHeroApi/SuperHeroApi/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿global using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+                
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=superherodb;Trusted_Connection=ture;TrustServerCertificate=true;");
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+
+    }
+}
--- SuperHeroApi/SuperHeroApi/Program.cs
+++ SuperHeroApi/SuperHeroApi/Program.cs
@@ -1,4 +1,5 @@
 global using SuperHeroApi.Models;
+using SuperHeroApi.Data;
 using SuperHeroApi.Services.SuperHeroService;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ISuperHeroService, SuperHeroService>();
+builder.Services.AddDbContext<DataContext>();
 
 
 var app = builder.Build();
