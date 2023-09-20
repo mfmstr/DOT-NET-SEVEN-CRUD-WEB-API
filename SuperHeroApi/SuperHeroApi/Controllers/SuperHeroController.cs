cd035ab2caaa8f99bbc187a754c47122ff24f4c3
mfmstr <g.kharebava01@gmail.com>
2023-09-20 18:18:18 +0400

    Adding Controller Class And First (GetAllHeroes) Method

---
+++ SuperHeroApi/SuperHeroApi/Controllers/SuperHeroController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroApi.Models;
+
+namespace SuperHeroApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+
+        private static List<SuperHero> superHeroes = new List<SuperHero>
+            {
+                new SuperHero
+                { Id = 1, Name = "Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+                return Ok(superHeroes);
+        }
+        
+    }
+}
